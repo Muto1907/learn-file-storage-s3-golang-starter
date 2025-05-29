7d008851ad82cd46e840142f3c9bcd65f14de823
Muto1907 <Mahmut.Cinar93@outlook.de>
2025-05-29 13:46:17 +0200

    Implement Uploading of Videothumbnails which are stored in the sqlite3 db as base64 strings

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,43 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+	thumbnailMediaType := header.Header.Get("Content-Type")
+	if thumbnailMediaType == "" {
+		respondWithError(w, http.StatusBadRequest, "Missing Content-Type Header in request", err)
+		return
+	}
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read image data", err)
+		return
+	}
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't find video", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized access to video", err)
+		return
+	}
+	image64 := base64.StdEncoding.EncodeToString(imageData)
+	dataURL := fmt.Sprintf("data:%s;base64,%s", thumbnailMediaType, image64)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	video.ThumbnailURL = &dataURL
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update videothumbnail URL", err)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, video)
 }
